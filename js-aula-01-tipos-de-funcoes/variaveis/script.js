6abf30deaf9d15b6752fe8cd41732a2def7b1d57
DSM2 <fatec-dsm2@academico.intra>
2026-02-11 20:55:11 -0300

    Aula 01 - Variaveis

---
+++ js-aula-01-tipos-de-funcoes/variaveis/script.js
@@ -0,0 +1,24 @@
+//COMENTARIO DE LINHA
+
+/* COMENTARIO DE BLOCO*/
+
+// ctrl + ; --> atalho para comentarios
+
+//VAR: No geral evite seu uso, pode não ser muito seguro.
+//LET: Utilize quando for necessario reatribuir o valor da variavel.
+//CONST: Utilize quando não precisar reatribuir o valor da variavel.
+
+var cidade = "Registro";
+var cidade = "Sete Barras";
+document.write(cidade);
+
+let nome = "Diego";
+nome = "Maria";
+document.write("<br>");
+document.write(nome);
+
+const pais = "Brasil";
+pais = "Argentina";
+// O valor de uma const não pde ser alterado
+document.write("<br>");
+document.write(pais);
